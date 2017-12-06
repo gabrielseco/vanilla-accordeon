2521dab26376dd8be28b73372c6af35675e73c01
gabriel <ggarciaseco@gmail.com>
2017-12-06 20:35:49 +0100

    Add applyClasses test

---
+++ test/utils/applyClasses.test.js
@@ -0,0 +1,30 @@
+import { applyClasses } from '../../src/utils';
+
+describe('Apply classes', () => {
+  it('should test the classes utility with one class', () => {
+    expect(applyClasses({
+      notification: true,
+      hidden: false,
+    })).toBe('notification');
+  });
+  it('should test the classes utility with zero class', () => {
+    expect(applyClasses({
+      notification: false,
+    })).toBe('');
+  });
+
+  it('should test the classes utility with two classes', () => {
+    expect(applyClasses({
+      notification: true,
+      hidden: true,
+    })).toBe('notification hidden');
+  });
+
+  it('should not generate spaces between classes', () => {
+    expect(applyClasses({
+      notification: true,
+      display: false,
+      hidden: true,
+    })).toBe('notification hidden');
+  });
+});
